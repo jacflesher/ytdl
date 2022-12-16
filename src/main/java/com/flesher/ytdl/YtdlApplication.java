d5f394426503fdb5e23bf39cbc5a3bee704c73f0
Jac Flesher <jacflesher@Jacs-MacBook-Air.local>
2022-12-16 14:36:06 -0500

    update

---
+++ src/main/java/com/flesher/ytdl/YtdlApplication.java
@@ -0,0 +1,13 @@
+package com.flesher.ytdl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtdlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtdlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flesher/ytdl/YtdlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flesher.ytdl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtdlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
